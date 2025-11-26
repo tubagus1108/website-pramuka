3f186a02d746b3da7e11cd252877af7d7f3ca988
MUHAMMAD TUBAGUS SANANTO <tubagussananto2000@gmail.com>
2025-11-26 09:09:22 +0700

    first commit

---
+++ database/migrations/2025_11_26_020652_create_profiles_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('type', ['sejarah_kwarda', 'visi_misi', 'renstra', 'sejarah_pramuka', 'scout_history']);
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
